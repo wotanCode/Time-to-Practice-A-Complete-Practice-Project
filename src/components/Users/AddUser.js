bdf3257c345cce659a1cf8013a9dda8d7f5c4191
Pedro Yanez <pedro_y91@outlook.com>
2021-12-28 15:51:00 -0300

    93/Managing the user input state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 
 import classes from './AddUser.module.css'
 
@@ -12,13 +13,13 @@ const AddUser = props => {
   }
   return (
     <Card className={classes.input}>
-    <form osSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"/>
-      <label htmlFor="age">Age (years)</label>
-      <input id="age" type="number"/>
-      <button type="submit">Add User</button>
-    </form>
+      <form osSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
   );
 };

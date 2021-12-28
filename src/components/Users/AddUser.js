8df41981e8df1f157a671a9fb51ee1fda2ec5bd5
Pedro Yanez <pedro_y91@outlook.com>
2021-12-28 16:06:46 -0300

    93/managing the user input State

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -8,16 +8,28 @@ import classes from './AddUser.module.css'
 
 const AddUser = props => {
 
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredage, setEnteredage] = useState('');
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value)
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredage(event.target.value)
+  }
+
   const addUserHandler = (event) => {
-    event.preventDefault();
+    event.preventDefault('');
+    console.log(enteredUsername, enteredage);
   }
   return (
     <Card className={classes.input}>
-      <form osSubmit={addUserHandler}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler}/>
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
